23084b39f215d941e55353ecfca56fc836759229
gbaumann <giulianobaumann@mailbox.org>
2020-06-24 12:01:57 +0200

    postgres backup service

--- stack-stop.sh
+++ stack-stop.sh
@@ -1,4 +1,5 @@
-
+# Backup soap service database
+docker exec -t oli_webservice_postgres pg_dump -c -U postgres soap_service > db/backups/single/soap_service_db_dump_`date +%d-%m-%Y"_"%H_%M_%S`.sql
 
 # Docker compose down
 docker-compose -f docker-compose.yml down
