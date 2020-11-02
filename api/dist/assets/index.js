0e76019f31a942e00e5250cad1b50faebc903203
MauroBono62 <maurobono62@googlemail.com>
2020-11-02 10:55:25 +0100

    Deleted the dist folder from the git repo

--- api/dist/assets/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var error_response_1 = require("./error-response");
-Object.defineProperty(exports, "errorResponses", { enumerable: true, get: function () { return error_response_1.errorResponses; } });
-//# sourceMappingURL=index.js.map
--- api/dist/auth/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var auth_1 = require("./auth");
-Object.defineProperty(exports, "isAuthorizedUser", { enumerable: true, get: function () { return auth_1.isAuthorizedUser; } });
-Object.defineProperty(exports, "isAuthorizedAdmin", { enumerable: true, get: function () { return auth_1.isAuthorizedAdmin; } });
-//# sourceMappingURL=index.js.map
